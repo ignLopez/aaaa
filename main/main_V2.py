f5cd54bf52d611968d53ee66286aab4791173166
Ignacio <ign.lopez.lopez@gmail.com>
2019-03-27 01:22:27 +0000

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	main/dadadw.py
    # avance modo report

--- main/main_V2.py
+++ main/main_V2.py
@@ -80,7 +80,6 @@ class MainWindow:
         self.subploti.set_title('Histórico')
         self.canvas = FigureCanvasTkAgg(self.fundaGraph,self.frameTable)
         self.canvas.show()
-
         self.canvas.get_tk_widget().pack(side='bottom', expand=True, fill='both', pady=5)
 
         self.botonReport.pack(side='bottom', expand=True, fill='both')
@@ -110,8 +109,6 @@ class MainWindow:
         self.newWindow = Toplevel(self.window)
         self.app = ModoReport(self.newWindow, window)
         self.window.withdraw()
-
-
 
     def get_data(self):
 
@@ -607,6 +604,8 @@ class LecturaInteligente:
 
 class ModoReport:
     def __init__(self, newWindow, window):
+        query="SELECT fecha,categoria,eur FROM facts_table WHERE tipo='Gasto'"
+        query2="SELECT fecha,categoria,eur FROM facts_table WHERE tipo='Gasto' AND categoria='alquiler'"
         self.db = 'HOST_CON.db'
         self.newWindow = newWindow
         self.window = window
@@ -614,12 +613,68 @@ class ModoReport:
         self.frameTable = LabelFrame(self.newWindow, borderwidth=3, relief='ridge', bg='blue')
 
         self.button1 = Button(self.frameBottons, text='Resfrescar Report', width=15)
+        self.fundaGraph = Figure(figsize=(10,5),dpi=100)
+        self.subploti1 = self.fundaGraph.add_subplot(221)
+        self.subploti1.axis('equal')
+        self.subploti2 = self.fundaGraph.add_subplot(222)
 
+        self.subploti3 = self.fundaGraph.add_subplot(223)
+
+        self.subploti4 = self.fundaGraph.add_subplot(224)
+
+
+        self.canvas = FigureCanvasTkAgg(self.fundaGraph,self.frameTable)
+        self.canvas.show()
+        self.canvas.get_tk_widget().pack(side='bottom', expand=True, fill='both', pady=5)
 
         self.button1.pack(side='bottom', expand=True, fill='both')
 
         self.frameBottons.grid(column=0, row=0, sticky="nsew")
         self.frameTable.grid(column=1, row=0, sticky="nsew")
+        self.datos=self.get_data2(query)
+        self.datos2=self.get_data2(query2)
+        self.get_pie(self.datos)
+        self.get_cat_time(self.datos2,'alquiler')
+
+
+    def get_data2(self,query):
+
+        db='C://Users//Ignacio//Desktop//baseDatos//HOST_CON.db'
+        cnx = sqlite3.connect(db)
+        data = pd.read_sql_query(query, cnx)
+        data['fecha']=pd.to_datetime(data['fecha'], format="%d/%m/%Y")
+        return  data
+
+    def get_pie(self,data):
+        def limpiar_eur(x):
+            if type(x)==float:
+                return x
+            else:
+                return float(x.replace(',','.'))
+        data['fecha']=pd.to_datetime(data['fecha'], format="%d/%m/%Y")
+        data['mes']=data['fecha'].apply(lambda x : x.month)
+        data['anio']=data['fecha'].apply(lambda x : x.year)
+        data['eur']=data['eur'].apply(lambda x: limpiar_eur(x))
+        tablita=data[['categoria','eur']].pivot_table('eur',index='categoria',aggfunc='sum')
+        self.subploti1.pie(tablita.sort_values(by='eur', ascending=False).head(5), labels=tablita.sort_values(by='eur', ascending=False).head(5).index, autopct='%1.1f%%',
+                                                                                                                          shadow=True, startangle=90)
+
+    def get_cat_time(self,data,cat):
+        def limpiar_eur(x):
+            if type(x)==float:
+                return x
+            else:
+                return float(x.replace(',','.'))
+        data['fecha']=pd.to_datetime(data['fecha'], format="%d/%m/%Y")
+        data['mes']=data['fecha'].apply(lambda x : x.month)
+        data['anio']=data['fecha'].apply(lambda x : x.year)
+        data['eur']=data['eur'].apply(lambda x: limpiar_eur(x))
+        tablita=data[data['categoria']==cat].pivot_table('eur',index=['anio','mes'],aggfunc='sum')
+        tablita.index=[ pd.datetime(anio,mes,1).date().strftime('%y/%m') for (anio ,  mes ) in tablita.index]
+        tablita=pd.DataFrame(tablita.to_records())
+        self.subploti2.plot(tablita['index'].sort_values(),tablita['eur'])
+        self.subploti2.set_xticklabels(tablita['index'].sort_values(),rotation=60)
+        print(tablita['index'].sort_values(by=tablita.index),tablita['eur'])
 
 if __name__ == '__main__':
     window = Tk()
